09d44bdeb3295ff57ccef09a69c998b5c820663f
Mark <mark@does.work>
2022-06-06 14:52:17 -0400

    hide show events

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
 console.log("mark was here");
 
-// Your CSS as text
+// Custom CSS for aesthetics
 var styles = `
 .bookmark {
         min-height: 0px;
@@ -8,16 +8,70 @@ var styles = `
 }
 .eevent-bookmark {
         display: none;
-}`
+}`;
 
 var styleSheet = document.createElement("style")
 styleSheet.innerText = styles
 document.head.appendChild(styleSheet)
+// Custom CSS for aesthetics
 
-const rooms = document.getElementsByClassName("bookmarks-subheader")[0];
+// hide/show events cheveron
+ var bookeventss = document.getElementsByClassName("event-bookmark");
+ if (bookeventss.length > 0) {
 
-const expander = document.createElement('a');
-         rooms.insertAdjacentHTML("afterbegin", '<div class="textclass" style="display:flex">');
+	const rooms = document.getElementsByClassName("bookmarks-section-header")[0];
 
+	var downHtml =
+								`<div id="downnn" class="textclass">
+									<link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
+										<style>
+											i {
+											color: #669FAB;
+											font-size: 34px !important;
+														}
+										</style>
+									<i class="material-icons">
+										expand_more
+									</i>
+								</div>
+	`;
+	var upHtml =
+								`<div id="uppp" class="textclass" style="display:none">
+									<link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
+										<style>
+											i {
+											color: #669FAB;
+											font-size: 34px !important;
+														}
+										</style>
+									<i class="material-icons">
+										expand_less
+									</i>
+								</div>
+	`;
 
- rooms.insertAdjacentHTML("afterend", '<link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet"> <style> i {color: #669FAB;font-size: 34px !important; /* 24px preferred icon size */}</style><i class="material-icons">expand_more</i></div>');
+	rooms.insertAdjacentHTML("afterend", downHtml);
+	rooms.insertAdjacentHTML("afterend", upHtml);
+	for (var i = 0; i < bookeventss.length; i ++) {
+		bookeventss[i].style.display = 'none';
+	};
+
+	document.getElementById("uppp").addEventListener("click", function(e) {
+		var bookevents = document.getElementsByClassName("event-bookmark");
+			for (var i = 0; i < bookevents.length; i ++) {
+				bookevents[i].style.display = 'none';
+			};
+		document.getElementById("downnn").style.display = 'block';
+		document.getElementById("uppp").style.display = 'none';
+	});
+
+	document.getElementById("downnn").addEventListener("click", function(e) {
+		var bookevents = document.getElementsByClassName("event-bookmark");
+			for (var i = 0; i < bookevents.length; i ++) {
+				bookevents[i].style.display = 'block';
+			};
+		document.getElementById("downnn").style.display = 'none';
+		document.getElementById("uppp").style.display = 'block';
+	});
+ };
+// hide/show events cheveron
